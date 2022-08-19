6dc62728334f15448a1104c6042f0e56659ed328
Brian Williams <heffergod@gmail.com>
2022-08-19 15:38:23 -0700

    I completely missed the sdlc section of the assessment, so I've filled that out now.

--- sdlc.js
+++ sdlc.js
@@ -5,26 +5,13 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
-let password = "asdsa133"
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
-for (i = 0; i < password.length; i++)
-{
-    if (!isNaN(password[i]))
-    {
-        //console.log(password[i])
-    }
-}
-
-if (/[a-zA-Z].test(password)/ && isNaN)
-{
-    console.log("yarp")
-}
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -32,3 +19,13 @@ if (/[a-zA-Z].test(password)/ && isNaN)
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This step defines the specific requirements needed in order to meet the end goal set out in the planning stage."
+
+const design = "This step models the way the application works, including archetecture, user interface, platforms, programming, comminucations, and security."
+
+const implementation = "This is stem in which the actual writing of the program occurs."
+
+const testingAndIntegration = "This step involves testing of the code to make sure it's both doign what it is supposed to and meeting the requirements laid out in previous steps"
+
+const maintain = "This step is an ongoing step meant to keep the code up-to-date and working after it has gone live and is being used.  It can include further bug fixes, further feature implementation, and optimization."
