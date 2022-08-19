d48e8cad0663b6451b74c857ff235b281d9a9238
Brian Williams <heffergod@gmail.com>
2022-08-19 11:07:00 -0700

    Fixed a few cases in which password.js didn't work by moving around several statements.

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
 // current password
 
-let password = "aASDASka230123фqw;a2d"
+let password = "hunterasda"
 
 // Creating gigantic arrays with letters and numbers seems like a gross way to do it, but it should work with the .includes() method.  I've included the .toUpperCase() method to check both upper and lowercase letters without the need of looping over an additional array of lowercase letters.
 
@@ -10,16 +10,15 @@ let letterChecker = false
 let numberChecker = false
 
 
+// For the for loop, I've innitially checked to make sure the password is at least 10 characters, logging a failure state and breaking the loop if it's not.  Next I check to see if each character is either a letter or a number, flipping the boolian value of the relevant Checker variable.  Next I created a seperate "if" statement to check if we've already logged both a character as well as a number, we return a successful password statement and break the loop.
+
+// Finally, I created a statement outside of the "for" loop that checked to see if either the letterChecker or the numberChecker were still in a false state.  If they were, a failure statement was given to the user.
+
 for (i = 0; i < password.length; i++)
 {
     if (password.length < 10)
     {
         console.log("Your password needs to be at least 10 characters in length, including both at least one letter and at least one number")
-        break
-    }
-    else if (letterChecker === true && numberChecker === true)
-    {
-        console.log("Your password is of a valid format!")
         break
     }
     else if (letters.includes(password[i].toUpperCase(password[i])))
@@ -29,9 +28,17 @@ for (i = 0; i < password.length; i++)
     else if (numbers.includes(password[i]))
     { 
         numberChecker = true
-    }
-    else if (i >= password.length - 1 && !letterChecker && !numberChecker)
+    } 
+
+    if (letterChecker && numberChecker)
     {
-        console.log("Your password needs to contain at least one letter and at least one number")
+        console.log("Your password is of a valid format!")
+        break
     }
 }
+if (!letterChecker || !numberChecker)
+    {   
+        {
+            console.log("Your password needs to contain at least one letter and at least one number")
+        }
+    }
